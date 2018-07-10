30a1d26afb7c3dbda205147af6167b12a602ca42
alichtman <aaronlichtman@gmail.com>
2018-07-09 23:54:31 -0400

    Fixed listbox

--- src/listbox.py
+++ src/listbox.py
@@ -1,5 +1,17 @@
 # Aaron Lichtman
 # Displays a list box with a set of choices.
+import urwid
+
+def item_chosen(button, choice):
+	response = urwid.Text([u'You chose ', choice, u'\n'])
+	done = urwid.Button(u'Ok')
+	urwid.connect_signal(done, 'click', exit_program)
+	main.original_widget = urwid.Filler(urwid.Pile([response, urwid.AttrMap(done, None, focus_map='reversed')]))
+
+
+def exit_program(button):
+	raise urwid.ExitMainLoop()
+
 
 def menu(title, choices):
 	"""
@@ -13,6 +25,11 @@ def menu(title, choices):
 	return urwid.ListBox(urwid.SimpleFocusListWalker(body))
 
 
-def main():
-    choices = ["Activate/Deactivate Automated Trading", "Deposit/Withdraw Money", "Place Your Own Trade"]
-    menu("Options", choices)
+if __name__ == '__main__':
+	choices = ["Choice 1", "Choice 2", "Choice 3"]
+	main = urwid.Padding(menu(u'Options', choices), left=2, right=2)
+	top = urwid.Overlay(main, urwid.SolidFill(u'\N{LIGHT SHADE}'),
+	                    align='left', width=('relative', 0),
+	                    valign='top', height=('relative', 0),
+	                    min_width=20, min_height=7)
+	urwid.MainLoop(top, palette=[('reversed', 'standout', '')]).run()
